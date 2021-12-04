c2528562ce9c93f1fac55f27db02629d9896f3b8
sh-2020 <2191523412@qq.com>
2021-12-04 16:58:53 +0800

    完成input组件

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,12 +1,183 @@
+import React, { useEffect, useState } from "react"
+import classes from 'classnames'
+import s from './style.module.scss'
 
 export interface InputProps {
-  type?: 'l-input' | 'm-input'
+  /**
+   * @description 类型
+   */
+  type?: 'text' | 'textarea' | ''
+  /**
+   * @description 大小
+   */
+  size?: 'normal' | 'large'
+  /**
+   * @description 自动获取焦点
+   */
+  autoFocus?: boolean
+  /**
+   * @description 是否禁用
+   */
+  disabled?: boolean
+  addonBefore?: React.ReactNode
+  addonAfter?: React.ReactNode
+  allowCount?: boolean
+  allowClear?: boolean
+  rows?: number
+  style?: React.CSSProperties
+  value?: string | number | undefined
+  placeholder?: string
+  id?: string
+  maxLength?: number
+  bgTransparent?: boolean
+  onChange?: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
 }
+const Input: React.FC<InputProps> = (props) => {
+  let input: HTMLInputElement | HTMLTextAreaElement | undefined 
+  const {
+    value,
+    style,
+    bgTransparent,
+    type,
+    addonBefore,
+    addonAfter,
+    allowCount = true,
+    allowClear,
+    onChange
+  } = props
+  const [focus, setFocus] = useState<boolean>(false)
+  const [text, setText] = useState<string | number | undefined>(value || '')
+  useEffect(()=>{
+    setText(value)
+  }, [])
 
-const Input: React.FC<InputProps> = () => {
+  useEffect(()=> {
+    setText(props.value)
+  }, [props])
+
+  const actionChange = (e: any) => {
+    const target = input as any
+    const event = Object.create(e)
+    // 如果是点击清除按钮，则改变target指向input， value清空
+    if (e.type === 'click') {
+      target.value = ''
+      event.target = target
+      event.currentTarget = target
+    }
+    onChange && onChange(event)
+  }
+  const focusEvent = () => {
+    input && input.focus()
+  }
+  const blurEvent = () => {
+    input && input.blur()
+  }
+  const onFocus = () => {
+    setFocus(true)
+  }
+  const onBlur = () => {
+    setFocus(false)
+  }
+  const onInput = (e: any) => {
+    setText(e.target.value)
+    actionChange(e)
+  }
+  /**
+   * @description 保存input节点
+   * @param inputs 
+   */
+  const saveInput = (inputs: HTMLInputElement | HTMLTextAreaElement) => {
+    input = inputs
+  }
+  const onClear = (e: any) => {
+    setText('')
+    actionChange(e)
+  }
+
+  const renderBefore = () => {
+    return !!addonBefore && <div className={s['my-input-center']}>{addonBefore}</div>
+  }
+  const renderAfter = () => {
+    return !! addonAfter && <div className={s['my-input-center']}>{addonAfter}</div>
+  }
+  const renderClear = () => {
+    return !!allowClear && !!text && <i className={s['my-input-clear']} onClick={onClear}></i>
+  }
+  const renderInput = () => {
+    const { placeholder, size = 'normal', maxLength, id, disabled, autoFocus } = props
+    return (
+      <div className={classes(s[`my-input-input`], {
+        [s[`${size}`]]: true
+      })}>
+        <input
+          {...{placeholder, maxLength, id, disabled, autoFocus}}
+          value={text}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onInput={onInput}
+          ref={saveInput as any}>
+        </input>
+        {renderClear()}
+      </div>
+    )
+  }
+  const renderTextarea = () => {
+    const { 
+      placeholder,
+      maxLength = 1000, 
+      id, 
+      disabled, 
+      autoFocus, 
+      rows } = props
+    const _rows = rows || 3
+    const _text = text
+    return (
+      <div
+        className={s['my-input-textarea']}
+        style={{height: 24 *_rows}}>
+        <textarea
+          {...{placeholder, maxLength, id, disabled, autoFocus}}
+          rows={_rows}
+          value={_text}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onInput={onInput}
+          ref={saveInput as any}>
+        </textarea>
+        {renderClear()}
+        {
+          allowCount && (
+            <div className={s['my-input-textarea-footer']}>
+              <span
+                className={classes({
+                  [s['max-length-text']]: !!maxLength && text && String(text).length >= maxLength
+                })}>
+                {String(text).length}
+              </span>
+              {!!maxLength && (
+                <>
+                  <span>/</span>
+                  <span>{maxLength}</span>
+                </>
+              )}
+            </div>
+          )
+        }
+      </div>
+    )
+  }
   return (
-    <div>
-      
+    <div
+      style={style}
+      className={classes(s['my-input'], {
+        normal: !bgTransparent,
+        focus: focus,
+        [s['allow-clear']]: allowClear
+      })}
+    >
+     {renderBefore()}
+     {type === 'textarea' ? renderTextarea() : renderInput()}
+     {renderAfter()}
     </div>
   )
 }
--- src/container/resume/index.tsx
+++ src/container/resume/index.tsx
@@ -1,17 +1,35 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Button from "../../components/button";
 import {ROUTE} from '../../routers'
+import Input from "../../components/input/input";
 const Resume = () => {
   
   const clickHandle = () => {
     console.log(123)
   }
+  let username
+  let hobby
   return (
+    
     <div>
       <div>我是Resume</div>
       <Button link to={ROUTE.info} onClick={clickHandle} size="middle">导出</Button>   
       <Button onClick={clickHandle} size="middle">导出</Button>  
+      <Input
+        value={username}
+        placeholder="请输入姓名" // 占位文本
+        allowClear={true} // 是否显示清除icon
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Input
+        value={hobby || ''}
+        placeholder="你有什么特长爱好呢" // 占位文本
+        allowClear={true} // 是否显示清除icon
+        onChange={(e) => console.log(e.target.value)}
+        type="textarea" // 类型为多行文本
+        rows={5} // 输入文本的行数
+        maxLength={200} // 最多支持的文本长度
+        allowCount={true} // 是否显示底部文本字数
+      />
     </div>
   )
 }
